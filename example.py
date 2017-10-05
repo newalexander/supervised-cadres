a466227cd2733c0ef583fd109074ff3e8ababdd6
Alexander New <alexander.new0@gmail.com>
2017-10-05 16:24:10 -0400

    Update example.py
    Added comparison models and expanded exposition and model analysis.

--- example.py
+++ example.py
@@ -4,14 +4,25 @@
 '''In this example, we're training a cadre model on the boston housing dataset.
 Information about the boston housing dataset can be found (e.g.)
 here: https://medium.com/@haydar_ai/learning-data-science-day-9-linear-regression-on-boston-housing-dataset-cd62a80775ef
-The regression task of this dataset is to predict the median value of occupied houses of a census tract
-in Boston, MA.'''
+The regression task of this dataset is to predict the median value of occupied houses of a census tract in Boston, MA.
+
+Our cadre model is chosen so that the cadre-assignment mechanism will be very
+sparse. We plot the distribution of cadre assignment and target prediction weights,
+along with the cadre centers.
+
+We also compare the predictive ability of a supervised cadre model with a linear SVR
+and a gaussian kernel SVR. The cadre model should outperform the linear SVR and be
+competitive with the nonlinear SVR, depending on vagaries of the train-validation split.
+
+Note that this script is written for Python 3.
+'''
 
 import numpy as np
 import scipy.stats as ss
 import pandas as pd
 import supCadresRegression as sc
 from sklearn.datasets import load_boston
+from sklearn.svm import LinearSVR, SVR
 import matplotlib.pyplot as plt
 
 #######################
@@ -32,11 +43,17 @@ Dva, Dtr = ss.zmap(Dva, Dtr), ss.zscore(Dtr)
 Xtr, Ytr = Dtr[:,:-1], np.expand_dims(Dtr[:,-1], 1)
 Xva, Yva = Dva[:,:-1], np.expand_dims(Dva[:,-1], 1)
 
-M = 3
+M = 3                # number of cadres
 alpha = [0.95, 0.05] # d is more l1, W is more l2
-lam = [0.01, 0.01]
+lam = [1, 1]         # regularization strength
 
 cadreModel = sc.learnCadreModel(Xtr, Ytr, Xva, Yva, M, alpha, lam, 1)
+
+## learn SVRs
+lsvr = LinearSVR(epsilon=0.1, C=1)
+lsvr.fit(Xtr, np.squeeze(Ytr))
+ksvr = SVR(kernel='rbf', C=2)
+ksvr.fit(Xtr, np.squeeze(Ytr))
 
 ## plot true y values vs. predicted y values in the test set,
 ## colored by cadre membership
@@ -44,17 +61,61 @@ colors = ['blue','red','green']
 for m in range(M):
     plt.scatter(Yva[np.where(cadreModel['mVa']==m)], 
                 cadreModel['fVa'][np.where(cadreModel['mVa']==m)], 
-                c=colors[m], label=m)
+                c=colors[m], label='cadre_'+str(m))
 plt.legend(loc='upper left')
+plt.xlabel('true Y')
+plt.ylabel('predicted Y')
+plt.show()
+
+## do the same for svr models
+plt.scatter(Yva, lsvr.predict(Xva), color='red', label='lsvr')
+plt.scatter(Yva, ksvr.predict(Xva), color='blue', label='ksvr')
+plt.legend(loc='upper left')
+plt.xlabel('true Y')
+plt.ylabel('predicted Y')
 plt.show()
 
 ## plot distribution of cadre assignment weights
-plt.plot(np.arange(P), cadreModel['d'])
+plt.plot(np.arange(P), np.abs(cadreModel['d']))
+plt.xlabel('feature index')
+plt.ylabel('cadre assignment weight')
+plt.show()
+
+## find out what features are used for cadre assignment
+threshold = 0.001
+print('features used for cadre assignment')
+print(boston.feature_names[np.where(np.abs(cadreModel['d']) > threshold)])
+print('only', np.sum(np.abs(cadreModel['d']) > threshold), 'out of', P, 'features are used for',
+      'cadre assignment at a threshold of', threshold)
+
+## plot distribution of cadre centers
+for m in range(M):
+    plt.plot(np.arange(P), cadreModel['C'][:,m], c=colors[m], label='cadre_'+str(m))
+plt.axhline(y=0, xmin=0, xmax=P, color='black', label='reference')
+plt.legend(loc='upper left')
+plt.xlabel('feature index')
+plt.ylabel('cadre center')
 plt.show()
 
 ## plot distribution of target prediction weights (i.e. linear models),
 ## colored by cadre
+## include linear svr for comparison
 for m in range(M):
-	plt.plot(np.arange(P), cadreModel['W'][:,m], c=colors[m], label=m)
+    plt.plot(np.arange(P), cadreModel['W'][:,m], c=colors[m], label='cadre_'+str(m))
+plt.plot(np.arange(P), lsvr.coef_, c='orange', label='lsvr')
+plt.axhline(y=0, xmin=0, xmax=P, color='black', label='reference')
 plt.legend(loc='upper left')
+plt.xlabel('feature index')
+plt.ylabel('linear model weight')
 plt.show()
+
+## calculate train set and test set MSE for each model
+print('supervised cadre training error, generalization error')
+print(np.mean((np.squeeze(Ytr) - cadreModel['fTr'])**2),
+      np.mean((np.squeeze(Yva) - cadreModel['fVa'])**2))
+print('linear SVR training error, generalization error')
+print(np.mean((np.squeeze(Ytr) - lsvr.predict(Xtr))**2),
+      np.mean((np.squeeze(Yva) - lsvr.predict(Xva))**2))
+print('nonlinear SVR training error, generalization error')
+print(np.mean((np.squeeze(Ytr) - ksvr.predict(Xtr))**2),
+      np.mean((np.squeeze(Yva) - ksvr.predict(Xva))**2))
