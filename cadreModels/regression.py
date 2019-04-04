d1869ee109f2d62a5b8afdde10b7456d08d0f058
Alexander New <alexander.new0@gmail.com>
2019-04-03 23:19:34 -0400

    interface based on pandas instead of numpy
    - Data should be specified as a pandas.DataFrame containing both features and target
    - Parameters estimates are returned as pandas.DataFrame objects for ease of plotting
    - Introduction of proximal gradient steps for more efficient incorporation of l1 penalty

--- cadreModels/regression.py
+++ cadreModels/regression.py
@@ -1,12 +1,14 @@
 ## regression.py
 ## scalar regression
 
-from __future__ import division, print_function, absolute_import
+## note that `calcMargiProb`, `calcJointProb`, `calcCondiProb`, and `estEntropy` aren't currently used in any methods.
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import itertools as it
 import scipy.special as ss
+import time
 
 def eNet(alpha, lam, v):
     """Elastic-net regularization penalty"""
@@ -45,8 +47,6 @@ class regressionCadreModel(object):
         self.lambda_W = lambda_W
         self.alpha_d = alpha_d    # elastic net mixing weights
         self.alpha_W = alpha_W    
-        self.cadFts = None        # cadre-assignment feature indices
-        self.tarFts = None        # target-prediction feature indices
         self.fitted = False
         ## optimization settings
         self.Tmax = Tmax     # maximum iterations
@@ -59,12 +59,15 @@ class regressionCadreModel(object):
         self.w0 = 0    # regression biases
         self.C = 0     # cadre centers
         self.d = 0     # cadre assignment weights
-        self.sigma = 0 # prediction noise
         ## data
-        self.X = None  # copy of input data
-        self.Y = None  # copy of target values
+        self.data = None  # copy of data
+        self.features = None # names of features
+        self.target = None # name of target
         ## outputs
         self.loss = [] # loss trajectory
+        self.mse = []  # MSE trajectory for training data
+        self.mseVa = [] # MSE trajectory for validation data
+        self.time = [] # optimization times by step
     
     def get_params(self, deep=True):
         return {'M': self.M, 'gamma': self.gamma, 'lambda_d': self.lambda_d, 
@@ -77,23 +80,24 @@ class regressionCadreModel(object):
             setattr(self, parameter, value)
         return self
         
-    def fit(self, Xtr, Ytr, cadFts=None, tarFts=None, inits=dict(), seed=16162):
+    def fit(self, Dtr, features, target, Dva=None, inits=dict(), seed=16162, progress=False, store=False):
+        np.random.seed(seed)
         """Fit regression cadre model"""
-        if cadFts is not None:
-            self.cadFts = cadFts
-        else:
-            self.cadFts = np.arange(Xtr.shape[1])
-            
-        if tarFts is not None:
-            self.tarFts = tarFts
-        else:
-            self.tarFts = np.arange(Xtr.shape[1])
-            
-        Pcad, Ptar, Ntr = len(self.cadFts), len(self.tarFts), Xtr.shape[0]
-        # number of ((cadre-assignment, target-prediction) features, training observations)
+        self.features = features
+        self.target = target
+        Ntr, P = Dtr.shape[0], features.shape[0] # number of training observations, number of features
         self.fitted = True
-        self.X = Xtr
-        self.Y = Ytr
+        
+        if store:
+            self.data = Dtr
+        
+        ## extract values from pd.DataFrames for faster access
+        data_mat = Dtr.loc[:,features].values
+        target_mat = Dtr.loc[:,[target]].values
+        
+        if Dva is not None:
+            data_mat_va = Dva.loc[:,features].values
+            target_mat_va = Dva.loc[:,[target]].values
         
         ############################################
         ## tensorflow parameters and placeholders ##
@@ -102,42 +106,36 @@ class regressionCadreModel(object):
     
         ## cadre centers parameter
         if 'C' in inits:
-            C = tf.Variable(inits['C'], dtype=tf.float64, name='C')
+            C = tf.Variable(inits['C'], dtype=tf.float32, name='C')
         else:
-            C = tf.Variable(np.random.normal(loc=0., scale=0.1, size=(Pcad,self.M)), 
-                            dtype=tf.float64, name='C')
+            C = tf.Variable(np.random.normal(loc=0., scale=0.1, size=(P,self.M)), 
+                            dtype=tf.float32, name='C')
         ## cadre determination weights parameter
         if 'd' in inits:
-            d = tf.Variable(inits['d'], dtype=tf.float64, name='d')
+            d = tf.Variable(inits['d'], dtype=tf.float32, name='d')
         else:
-            d = tf.Variable(np.random.uniform(size=(Pcad)), dtype=tf.float64, name='d')
+            d = tf.Variable(np.random.uniform(size=(P,)), dtype=tf.float32, name='d')
         ## regression hyperplane weights parameter
         if 'W' in inits:
-            W = tf.Variable(inits['W'], dtype=tf.float64, name='W')
+            W = tf.Variable(inits['W'], dtype=tf.float32, name='W')
         else:
-            W = tf.Variable(np.random.normal(loc=0., scale=0.1, size=(Ptar,self.M)), 
-                            dtype=tf.float64, name='W')
+            W = tf.Variable(np.random.normal(loc=0., scale=0.1, size=(P,self.M)), 
+                            dtype=tf.float32, name='W')
         ## regression hyperplane bias parameter
         if 'w0' in inits:
-            w0 = tf.Variable(inits['w0'], dtype=tf.float64, name='w0')
+            w0 = tf.Variable(inits['w0'], dtype=tf.float32, name='w0')
         else:
-            w0 = tf.Variable(tf.zeros(shape=(self.M,), dtype=tf.float64), 
-                             dtype=tf.float64, name='w0')
-        ## model error parameter
-        if 'sigma' in inits:
-            sigma = tf.Variable(inits['sigma'], dtype=tf.float64, name='sigma')
-        else:
-            sigma = tf.Variable(0.1, dtype=tf.float64, name='sigma')
+            w0 = tf.Variable(tf.zeros(shape=(self.M,), dtype=tf.float32), 
+                             dtype=tf.float32, name='w0')
     
-        Xcad = tf.placeholder(dtype=tf.float64, shape=(None,Pcad), name='Xcad')
-        Xtar = tf.placeholder(dtype=tf.float64, shape=(None,Ptar), name='Xtar')
-        N = tf.cast(tf.gather(tf.shape(Xcad), 0), dtype=tf.float64, name='N')
-        Y = tf.placeholder(dtype=tf.float64, shape=(None,1), name='Y')
+        X = tf.placeholder(dtype=tf.float32, shape=(None,P), name='X')
+        Y = tf.placeholder(dtype=tf.float32, shape=(None,1), name='Y')
+        eta = tf.placeholder(dtype=tf.float32, shape=(), name='eta')
         
         ## T[n,m] = ||x^n - c^m||^2_D
         T = tf.einsum('npm,p->nm', 
-                  tf.square(tf.map_fn(lambda x: tf.expand_dims(x,1) - C, Xcad)), 
-                  tf.abs(d))
+                  tf.square(tf.map_fn(lambda x: tf.expand_dims(x,1) - C, X)), 
+                  d)
     
         ## G[n,m] = g_m(x^n)
         ##        = 1 / sum_m' exp(gamma(T[n,m] - T[n,m']))
@@ -146,56 +144,82 @@ class regressionCadreModel(object):
                                              tf.expand_dims(t,0))), axis=1), T, name='G')                 
     
         ## E[n,m] = e_m(x^n)
-        E = tf.add(tf.matmul(Xtar, W), w0, name='E')
+        E = tf.add(tf.matmul(X, W), w0, name='E')
+        
+        ## F[n] = f(x^n) = sum_m g_m(x^n) e_m(x^n)
+        F = tf.reduce_sum(G * E, axis=1, keepdims=True)
     
         ## L = 1 / N sum_n sum_m g_m(x^n) * (e_m(x^n) - y_n) ^2
-        L = tf.add(tf.reduce_mean(tf.reduce_sum(G * (E - Y) **2, axis=1)) / 2 / sigma**2,
-             (eNet(self.alpha_d, self.lambda_d, d) + 
-              eNet(self.alpha_W, self.lambda_W, W)) / 2 / N / sigma**2 +
-             (1/2 + 1/N) * tf.log(sigma**2), name='L')
-    
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.eta).minimize(L)
+        MSE = tf.reduce_mean( (F - Y)**2 )
+        l2_d = self.lambda_d * (1 - self.alpha_d) * tf.reduce_sum(d**2)
+        l2_W = self.lambda_W * (1 - self.alpha_W) * tf.reduce_sum(W**2)
+        l1_d = self.lambda_d * self.alpha_d * tf.reduce_sum(tf.abs(d))
+        l1_W = self.lambda_W * self.alpha_W * tf.reduce_sum(tf.abs(W))
+        l2_C = 1e-7 * tf.reduce_sum(C**2)
+        
+        loss_smooth = MSE + l2_d + l2_W + l2_C
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.eta).minimize(loss_smooth)
+        loss_full = loss_smooth + l1_d + l1_W
+        
+        ## proximal gradient steps
+        thresh_W = tf.assign(W, tf.sign(W) * (tf.abs(W) - eta * self.lambda_W * self.alpha_W) * tf.cast(tf.abs(W) > eta * self.lambda_W * self.alpha_W, tf.float32))
+        thresh_d = tf.assign(d, tf.maximum(0., tf.sign(d) * (tf.abs(d) - eta * self.lambda_d * self.alpha_d) * tf.cast(tf.abs(d) > eta * self.lambda_d * self.alpha_d, tf.float32)))
         
         ####################
         ## learning model ##
         ####################
         with tf.Session() as sess:
             tf.global_variables_initializer().run()
+            t0 = time.time()
             
             ## perform optimization
             for t in range(self.Tmax):
-                inds = np.random.choice(Ntr, self.Nba, replace=False)      
-                sess.run(optimizer, feed_dict={Xcad: Xtr[np.ix_(inds, self.cadFts)],
-                                               Xtar: Xtr[np.ix_(inds, self.tarFts)],
-                                               Y: Ytr[inds]})
+                inds = np.random.choice(Ntr, self.Nba, replace=False)  
+                ## take gradient step
+                sess.run(optimizer, feed_dict={X: data_mat[inds,:],
+                                               Y: target_mat[inds,:]})
+                ## take proximal step
+                sess.run([thresh_d, thresh_W], feed_dict={eta: self.eta})
                 # record-keeping        
                 if not t % self.record:
-                    self.loss.append(L.eval(feed_dict={
-                            Xcad: Xtr[:,self.cadFts],
-                            Xtar: Xtr[:,self.tarFts],
-                            Y: Ytr}))
-                    if len(self.loss) > 2 and (np.abs(self.loss[-1] - self.loss[-2]) < self.eps):
-                        break   
-            self.C, self.d, self.W, self.w0, self.sigma = C.eval(), d.eval(), W.eval(), w0.eval(), sigma.eval()    
+                    [l, mse_tr] = sess.run([loss_full, MSE], feed_dict={X: data_mat, Y: target_mat})
+                    self.loss.append(l)
+                    self.mse.append(mse_tr)
+                    if Dva is not None:
+                        mse_va = MSE.eval(feed_dict={X: data_mat_va, Y: target_mat_va})
+                        self.mseVa.append(mse_va)
+                    self.time.append(time.time() - t0)
+                    
+                    if progress:
+                        if len(self.time) and Dva is not None:
+                            print(t, self.loss[-1], self.mse[-1], self.mseVa[-1], self.time[-1])
+                        elif len(self.time):
+                            print(t, self.loss[-1], self.mse[-1], self.time[-1])
+                        else:
+                            print(t)
+                    
+            self.C, self.d, self.W, self.w0 = C.eval(), d.eval(), W.eval(), w0.eval()
+            self.C = pd.DataFrame(self.C, index=self.features)
+            self.d = pd.DataFrame(self.d, index=self.features)
+            self.W = pd.DataFrame(self.W, index=self.features)
             
         return self
     
-    def predictFull(self, Xnew):
+    def predictFull(self, Dnew):
         """Returns predicted values, cadre weights, and cadre estimates for new data"""
         if not self.fitted: print('warning: model not yet fit')
         
         tf.reset_default_graph()
-        C  = tf.Variable(self.C, dtype=tf.float64, name='C')
-        d  = tf.Variable(self.d, dtype=tf.float64, name='d')
-        W  = tf.Variable(self.W, dtype=tf.float64, name='W')
+        C  = tf.Variable(self.C.values, dtype=tf.float64, name='C')
+        d  = tf.Variable(self.d.values[:,0], dtype=tf.float64, name='d')
+        W  = tf.Variable(self.W.values, dtype=tf.float64, name='W')
         w0 = tf.Variable(self.w0, dtype=tf.float64, name='w0')
-        Xcad = tf.placeholder(dtype=tf.float64, shape=(None,len(self.cadFts)), name='X')
-        Xtar = tf.placeholder(dtype=tf.float64, shape=(None,len(self.tarFts)), name='X')
+        X = tf.placeholder(dtype=tf.float64, shape=(None,len(self.features)), name='X')
         
         ## T[n,m] = ||x^n - c^m||^2_D
         T = tf.einsum('npm,p->nm', 
-                  tf.square(tf.map_fn(lambda x: tf.expand_dims(x,1) - C, Xcad)), 
-                  tf.abs(d))
+                  tf.square(tf.map_fn(lambda x: tf.expand_dims(x,1) - C, X)), 
+                  d)
     
         ## G[n,m] = g_m(x^n)
         ##        = 1 / sum_m' exp(gamma(T[n,m] - T[n,m']))
@@ -204,7 +228,7 @@ class regressionCadreModel(object):
                                     tf.expand_dims(t,0))), axis=1), T, name='G')                 
     
         ## E[n,m] = e_m(x^n)
-        E = tf.add(tf.matmul(Xtar, W), w0, name='E')
+        E = tf.add(tf.matmul(X, W), w0, name='E')
         
         ## f[n] = f(x^n)
         F = tf.reduce_sum(G * E, axis=1, name='F') # this won't work if minibatch size 1 is used
@@ -212,44 +236,14 @@ class regressionCadreModel(object):
         
         with tf.Session() as sess:
             tf.global_variables_initializer().run()
-            Fnew, Gnew, mNew = sess.run([F, G, bstCd], feed_dict={Xcad: Xnew[:,self.cadFts],
-                                                                  Xtar: Xnew[:,self.tarFts]})
+            Fnew, Gnew, mNew = sess.run([F, G, bstCd], feed_dict={X: Dnew[self.features].values})
         return Fnew, Gnew, mNew
     
-        
-    def predict(self, Xnew):
+    def predict(self, Dnew):
         """Returns predicted values for new data"""
-        return self.predictFull(Xnew)[0]
+        return self.predictFull(Dnew)[0]
     
-    def score(self, Xnew, Ynew):
+    def score(self, Dnew):
         """Returns average sum-of-squares for new data"""
-        Fnew = self.predict(Xnew)
-        return ((Fnew - np.squeeze(Ynew))**2).mean()
-    
-    def entropy(self, Xnew):
-        """Returns estimated entropy for each cadre"""
-        G, m = self.predictFull(Xnew)[1:]    
-        marg = calcMargiProb(m, self.M)
-        jont = calcJointProb(G, m,  self.M)
-        cond = calcCondiProb(jont, marg)
-        return estEntropy(cond)
-    
-    def getNumberParams(self):
-        """Returns number of parameters of a model"""
-        return np.prod(self.C.shape) + np.prod(self.d.shape) + np.prod(self.W.shape) + np.prod(self.w0.shape)
-
-    def getNumberParamsRed(self, threshold=1e-4):
-        """Returns number of active parameters of a model"""
-        if not self.fitted: print('warning: model not yet fit')
-        return (np.sum(np.abs(self.C) > threshold) + np.sum(np.abs(self.d) > threshold) + 
-                np.sum(np.abs(self.W) > threshold) + np.sum(np.abs(self.w0 > threshold)))
-    
-    def calcBIC(self):
-        """Returns BIC of learned model"""
-        if not self.fitted: print('warning: model not yet fit')
-        return 2*self.Y.shape[0]*self.loss[-1] + 2 * self.getNumberParams() * np.log(self.Y.shape[0])
-
-    def calcBICred(self, threshold=1e-4):
-        """Returns effective degree-of-freedom BIC of learned model"""
-        if not self.fitted: print('warning: model not yet fit')
-        return 2*self.Y.shape[0]*self.loss[-1] + 2 * self.getNumberParamsRed(threshold) * np.log(self.Y.shape[0])
+        Fnew = self.predict(Dnew)
+        return ((Fnew - Dnew[self.target].values)**2).mean()
