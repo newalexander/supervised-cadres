50b6312ec550cc7d806aedc7a5af7e2fc78bf172
Alexander New <alexander.new0@gmail.com>
2019-06-24 15:25:54 -0400

    Update classificationMulti.py

--- cadreModels/classificationMulti.py
+++ cadreModels/classificationMulti.py
@@ -139,36 +139,131 @@ class multilabelCadreModel(object):
         F = tf.einsum('nm,nkm->nk', G, E, name='F')
         Yhat = tf.argmax(F, axis=1)
         
-        ## L = 1 / N sum_n log(p(y[n] | x[n])) + reg(Theta)
-        L = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=Y, logits=F)) + eNet(self.alpha_d, self.lambda_d, d) + eNet(self.alpha_W, self.lambda_W, W)
-        opt = tf.train.AdamOptimizer(learning_rate=self.eta)
-        optimizer = opt.minimize(L)
+        ## observation-wise error terms (based on jensen's inequality)
+        error_terms = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=Y, logits=F)
+        loss_score = tf.reduce_mean(error_terms)
+        
+        ## regularization
+        l2_d = self.lambda_d * (1 - self.alpha_d) * tf.reduce_sum(d**2)
+        l2_W = self.lambda_W * (1 - self.alpha_W) * tf.reduce_sum(lambda_Ws * W**2)
+        l1_d = self.lambda_d * self.alpha_d * tf.reduce_sum(tf.abs(d))
+        l1_W = self.lambda_W * self.alpha_W * tf.reduce_sum(lambda_Ws * tf.abs(W))
+        l2_C = 1e-7 * tf.reduce_sum(C**2)
+        
+        ## loss that is fed into optimizer
+        loss_opt = loss_score + l2_d + l2_W + l1_d + l1_W + l2_C
+        ## full loss, including l1 terms handled with proximal gradient
+        loss_full = loss_opt + l1_d + l1_W
+        optimizer = tf.train.AdamOptimizer(learning_rate=eta).minimize(loss_opt)
+        
+        ## nonsmooth proximal terms
+        thresh_W = tf.assign(W, tf.sign(W) * (tf.abs(W) - eta * self.lambda_W * lambda_Ws * self.alpha_W) * tf.cast(tf.abs(W) > eta * self.lambda_W * self.alpha_W, tf.float32))
+        thresh_d = tf.assign(d, tf.maximum(0., tf.sign(d) * (tf.abs(d) - eta * self.lambda_d * self.alpha_d) * tf.cast(tf.abs(d) > eta * self.lambda_d * self.alpha_d, tf.float32)))
         
         ####################
         ## learning model ##
         ####################
         with tf.Session() as sess:
             tf.global_variables_initializer().run()
-
+            
+            if progress:
+                if dataVa is not None:
+                    print('numbers being printed:', 
+                          'SGD iteration, training loss, training accuracy, validation loss, validation accuracy, time')
+                else:
+                    print('numbers being printed:',
+                          'SGD iteration, training loss, training accuracy, time')
+            
             t0 = time.time()
             ## perform optimization
             for t in range(self.Tmax):
                 inds = np.random.choice(Ntr, self.Nba, replace=False)
-                sess.run(optimizer, feed_dict={X: Xtr[inds,:], Y: Ytr[inds]})
+                ## calculate adaptive regularization parameter
+                cadres = bstCd.eval(feed_dict={Xcadre: dataCadre[inds,:], Xpredict: dataPredict[inds,:]})
+                cadre_counts = np.zeros(self.M)
+                for m in range(self.M):
+                    cadre_counts[m] = np.sum(cadres == m) + 1
+                cadre_counts = cadre_counts.sum() / cadre_counts
+                
+                ## take SGD step
+                sess.run(optimizer, feed_dict={Xcadre: dataCadre[inds,:],
+                                               Xpredict: dataPredict[inds,:],
+                                               Y: target_tr[inds,:],
+                                               lambda_Ws: cadre_counts,
+                                               eta: self.eta / np.sqrt(t+1)})
+                ## take proximal gradient step
+                sess.run([thresh_d, thresh_W], feed_dict={eta: self.eta / np.sqrt(t+1), lambda_Ws: cadre_counts})
                 # record-keeping        
                 if not t % self.record:
-#                     if len(self.time):
-#                         print(t, self.loss[-1], self.accs[-1], self.accsVa[-1])
-#                     else:
-#                         print(t)
+                    if progress:
+                        if len(self.time) and dataVa is not None:
+                            print(t,
+                                  self.metrics['training']['loss'][-1], 
+                                  self.metrics['training']['accuracy'][-1],
+                                  self.metrics['validation']['loss'][-1], 
+                                  self.metrics['validation']['accuracy'][-1],
+                                  self.time[-1])
+                        elif len(self.time):
+                            print(t,
+                                  self.metrics['training']['loss'][-1], 
+                                  self.metrics['training']['accuracy'][-1],
+                                  self.time[-1])
+                        else:
+                            print(t)
+
                     self.time.append(time.time() - t0)
-                    l, yhats = sess.run([L, Yhat], feed_dict={X: Xtr, Y: Ytr})
-                    self.loss.append(l)
-                    self.accs.append(np.mean(yhats == Ytr))
-                    if Xva is not None:
-                        yhats = Yhat.eval(feed_dict={X: Xva, Y: Yva})
-                        self.accsVa.append(np.mean(yhats == Yva))
-            self.C, self.d, self.W, self.W0 = C.eval(), d.eval(), W.eval(), W0.eval()  
+                    ## calculate metrics -- this should be its own function since it gets repeated
+                    cadres = bstCd.eval(feed_dict={Xcadre: dataCadre, Xpredict: dataPredict})
+                    cadre_counts = np.zeros(self.M)
+                    for m in range(self.M):
+                        cadre_counts[m] = np.sum(cadres == m) + 1
+                    cadre_counts = cadre_counts / cadre_counts.sum()
+                    self.metrics['training']['loss'].append(l)
+                    self.metrics['training']['accuracy'].append(np.mean(yhat == dataTarget))
+                    self.proportions.append(pd.Series(cadres).value_counts().T)
+                    self.proportions[-1] /= self.proportions[-1].sum()
+                    
+                    if dataVa is not None:
+                        cadres = bstCd.eval(feed_dict={Xcadre: dataCadreVa, Xpredict: dataPredictVa})
+                        cadre_counts = np.zeros(self.M)
+                        for m in range(self.M):
+                            cadre_counts[m] = np.sum(cadres == m) + 1
+                        cadre_counts = cadre_counts / cadre_counts.sum()
+                        l, margin = sess.run([loss_full, F], feed_dict={Xcadre: dataCadreVa,
+                                                                        Xpredict: dataPredictVa,
+                                                                        lambda_Ws: cadre_counts,
+                                                                        Y: target_va})
+                        self.metrics['validation']['loss'].append(l)
+                        self.metrics['validation']['accuracy'].append(np.mean(yhat == dataTargetVa))
+                        
+                    if dataVa is not None:
+                        if len(self.time) > 1:
+                            last_metric = self.metrics['validation'][self.termination_metric][-1]
+                            second_last_metric = self.metrics['validation'][self.termination_metric][-2]
+                            if np.abs(last_metric - second_last_metric) < self.eps:
+                                self.termination_reason = 'lack of sufficient decrease in validation ' + self.termination_metric
+                                break
+                    else:
+                        if len(self.time) > 1:
+                            last_metric = self.metrics['training'][self.termination_metric][-1]
+                            second_last_metric = self.metrics['training'][self.termination_metric][-2]
+                            if np.abs(last_metric - second_last_metric) < self.eps:
+                                self.termination_reason = 'lack of sufficient decrease in training ' + self.termination_metric
+                                break
+            if self.termination_reason == None:
+                self.termination_reason = 'model took ' + str(self.Tmax) + ' SGD steps'
+            if progress:
+                print('training has terminated because: ' + str(self.termination_reason))
+            self.C, self.d, self.W, self.W0 = C.eval(), d.eval(), W.eval(), W0.eval()
+            self.C = pd.DataFrame(self.C, index=self.cadreFts)
+            self.d = pd.Series(self.d, index=self.cadreFts)
+            self.W = pd.DataFrame(self.W, index=self.predictFts)
+            
+            ## clean up output for easier analysis
+            self.metrics['training'] = pd.DataFrame(self.metrics['training'])
+            if dataVa is not None:
+                self.metrics['validation'] = pd.DataFrame(self.metrics['validation'])
+            self.proportions = pd.concat(self.proportions, axis=1).T
             
         return self
     
@@ -177,16 +272,18 @@ class multilabelCadreModel(object):
         if not self.fitted: print('warning: model not yet fit')
         
         tf.reset_default_graph()
-        C  = tf.Variable(self.C, dtype=tf.float32, name='C')
-        d  = tf.Variable(self.d, dtype=tf.float32, name='d')
-        W  = tf.Variable(self.W, dtype=tf.float32, name='W')
+        C  = tf.Variable(self.C.values, dtype=tf.float32, name='C')
+        d  = tf.Variable(self.d.values, dtype=tf.float32, name='d')
+        W  = tf.Variable(self.W.values, dtype=tf.float32, name='W')
         W0 = tf.Variable(self.W0, dtype=tf.float32, name='w0')
-        X = tf.placeholder(dtype=tf.float32, shape=(None,Xnew.shape[1]), name='X')
+        Xcadre = tf.placeholder(dtype=tf.float32, shape=(None,self.cadreFts.shape[0]), name='Xcadre')
+        Xpredict = tf.placeholder(dtype=tf.float32, shape=(None,self.predictFts.shape[0]), name='Xpredict')
+        Y = tf.placeholder(dtype=tf.float32, shape=(None,1), name='Y')
         
         ## T[n,m] = ||x^n - c^m||^2_D
         T = tf.einsum('npm,p->nm', 
-                  tf.square(tf.map_fn(lambda x: tf.expand_dims(x,1) - C, X)), 
-                  tf.abs(d))
+              tf.square(tf.map_fn(lambda x: tf.expand_dims(x,1) - C, Xcadre)), 
+              d)
     
         ## G[n,m] = g_m(x^n)
         ##        = 1 / sum_m' exp(gamma(T[n,m] - T[n,m']))
@@ -200,10 +297,68 @@ class multilabelCadreModel(object):
         ## F[n,y] = f_y(x^n)
         F = tf.einsum('nm,nym->ny', G, E, name='F')
         Yhat = tf.argmax(F, axis=1)
-    
         bstCd = tf.argmax(G, axis=1, name='bestCadre')
+        
+        ## observation-wise error terms (based on jensen's inequality)
+        error_terms = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=Y, logits=F)
+        loss_score = tf.reduce_mean(error_terms)
+        
+        ## regularization
+        l2_d = self.lambda_d * (1 - self.alpha_d) * tf.reduce_sum(d**2)
+        l2_W = self.lambda_W * (1 - self.alpha_W) * tf.reduce_sum(lambda_Ws * W**2)
+        l1_d = self.lambda_d * self.alpha_d * tf.reduce_sum(tf.abs(d))
+        l1_W = self.lambda_W * self.alpha_W * tf.reduce_sum(lambda_Ws * tf.abs(W))
+        l2_C = 1e-7 * tf.reduce_sum(C**2)
+        
+        loss_full = loss_score + l2_d + l2_W + l2_C + l1_d + l1_W
         
         with tf.Session() as sess:
             tf.global_variables_initializer().run()
-            Fnew, Lnew, Gnew, mNew = sess.run([F, Yhat, G, bstCd], feed_dict={X: Xnew})
-        return Fnew, Lnew, Gnew, mNew
+            Fnew, Lnew, Gnew, mNew, loss = sess.run([F, Yhat, G, bstCd, loss_full], 
+                                                     feed_dict={Xcadre: Dnew.loc[:,self.cadreFts].values,
+                                                                Xpredict: Dnew.loc[:,self.predictFts].values,
+                                                                Y: Dnew.loc[:,[self.targetCol]]})
+        return Fnew, Lnew, Gnew, mNew, loss
+    
+    def predictMargin(self, Dnew):
+        """Returns classification scores for new data"""
+        Fnew, __, __, __, __ = self.predictFull(Dnew)
+        return Fnew
+
+    def predictClass(self, Dnew):
+        """Returns predicted labels for new data"""
+        __, Lnew, __, __, __ = self.predictFull(Dnew)
+        return Lnew
+    
+    def predictCadre(self, Dnew):
+        """Returns predicted cadre for new data"""
+        __, __, __, mNew, __ = predictFull(Dnew)
+        return mNew
+    
+    def entropy(self, Xnew):
+        """Returns estimated entropy for each cadre"""
+        __, __, G, m, __ = self.predictFull(Xnew)   
+        marg = u.calcMargiProb(m, self.M)
+        jont = u.calcJointProb(G, m,  self.M)
+        cond = u.calcCondiProb(jont, marg)
+        return u.estEntropy(cond)
+    
+    def weight_comparison(self):
+        """Returns estimate of between-cadre prediction weight diversity: 1 / P \sum_p StdDev(w^1_p, ..., w^M_p)"""
+        return self.W.std(axis=1).mean()
+    
+    def score(self, Dnew):
+        """Returns classification rate for new data"""
+        target = Dnew.loc[:,[self.targetCol]].values
+        Lnew = self.predictClass(Dnew)
+        return np.mean(target == Lnew)
+    
+    def scoreMetrics(self, Dnew):
+        """Returns goodness-of-fit metrics for new data as pd.DataFrame"""
+        target = Dnew.loc[:,[self.targetCol]].values        
+        margin, label, __, __, loss = self.predictFull(Dnew)
+        
+        accuracy = np.mean(target == label)
+        
+        return pd.DataFrame({'loss': loss,
+                             'accuracy': accuracy}, index=[0])
